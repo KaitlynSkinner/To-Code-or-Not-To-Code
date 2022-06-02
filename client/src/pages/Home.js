20fa15da79d0e9feb16c30d4650e0bad6ec416a5
wctestbank <wc.businessmail@gmail.com>
2022-06-02 03:49:02 -0400

    front page work

---
+++ client/src/pages/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { useMutation, useQuery } from '@apollo/client';
+
+import Auth from '../utils/auth';
