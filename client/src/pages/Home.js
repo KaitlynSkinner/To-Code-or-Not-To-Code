4dbad1af10c892e7ae3acbb3d7ac18d2735266c1
wctestbank <wc.businessmail@gmail.com>
2022-06-02 17:29:14 -0400

    Home page for client added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,43 @@
 import React from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-import { useMutation, useQuery } from '@apollo/client';
+import { Button, InputGroup, FormControl } from 'react-bootstrap';
 
-import Auth from '../utils/auth';
+function Home() {
+    return (
+        <section className="my-5">
+            <h1 id="about">My Bootcamp Experience</h1>
+            <div className="my-2">
+
+                <InputGroup id='about' size='lg' className="mb-3">
+                    <InputGroup.Text>About Me:</InputGroup.Text>
+                    <FormControl as="textarea" aria-label="With textarea" />
+                </InputGroup>
+
+                <InputGroup id='experience' size='lg' className="mb-3">
+                    <InputGroup.Text>Boot Camp Experience:</InputGroup.Text>
+                    <FormControl as="textarea" aria-label="With textarea" />
+                </InputGroup>
+
+                <InputGroup id='tips' size='lg' className="mb-3">
+                    <InputGroup.Text>Tips and Tricks:</InputGroup.Text>
+                    <FormControl as="textarea" aria-label="With textarea" />
+                </InputGroup>
+
+                <InputGroup id='connections' size='sm' className="mb-3">
+                    <InputGroup.Text>Connections:</InputGroup.Text>
+                    <FormControl as="textarea" aria-label="With textarea" />
+                </InputGroup>
+
+
+                <InputGroup id='institution' size='sm' className="mb-3">
+                    <InputGroup.Text>University/Institution:</InputGroup.Text>
+                    <FormControl as="textarea" aria-label="With textarea" />
+                </InputGroup>
+
+                <Button>Submit</Button>
+
+            </div>
+        </section>
+    );
+}
+
+export default Home;
